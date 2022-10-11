cb28764611901b26e11f927266ef648f6e572adf
Eurin HASH <eurindelagoldcoast@gmail.com>
2022-10-11 15:18:02 +0200

    Exercice sur les operation

--- super-projet/main.c
+++ super-projet/main.c
@@ -3,10 +3,20 @@
 
 double main()
 {
-    // la division
-    double resultat = 0;
-    resultat = 5.0 / 2.0;
-    printf("5 / 2 = %f", resultat);
+   // calcul entre variable
+   int resultat = 0 , nombre1 = 0, nombre2 = 0;
+   // on demande à l'utilisateur d'entrer les nombres 1 et 2
+   printf("Entrez un nombre 1 : ");
+   scanf("%d", &nombre1);
+   printf("Entrez un nombre 2 : ");
+   scanf("%d", &nombre2);
+   // on fait  le calcule
+
+   resultat = nombre1 + nombre2;
+
+   // on affiche le resultat
+
+   printf("%d + %d = %d\n", nombre1, nombre2, resultat);
     return 0;
 }
 
